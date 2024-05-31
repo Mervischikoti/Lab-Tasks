569948ad5a1fa46fa42c67eef72bed4e649b5a29
Mervischikoti <bsc-ele-32-22@unima.ac.mw>
2024-06-01 11:28:36 +1300

    Lab tasks assignment

---
+++ task6/classrectandgel.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+class rectangel{
+    private:
+    float length;
+    float width;
+
+    public:
+    void setrectangel( float length,float width );
+    float length Getrectangel();
+};
---
+++ task6/lab6exercise.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+# include<string>
+using namespace std;
+
+int main(){
+    cout<<"enter avariable"<<endl;
+        int *var =new int;
+        cin>> *var;
+        cout<<"write aword of your choice"<<endl;
+        string * word =new string;
+        cin>>*word;
+        cout<<"your valeu is "<<*var<<" "<<"your word is "<< " is "<<*word<<endl;
+        delete word;
+        word = nullptr;
+
+}
